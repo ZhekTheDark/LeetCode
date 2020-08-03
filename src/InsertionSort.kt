967ccea5756e37f18f8bcdb97e3ac0054dc5ddb6
Eugene <Zhekfake@gmail.com>
2020-08-03 22:05:32 +0300

    MergeSort added

--- src/InsertionSort.kt
+++ src/InsertionSort.kt
@@ -8,7 +8,8 @@
  * Space complexity : O(1).
  */
 
-fun insertionSort(array: IntArray) {
+private fun insertionSort(array: IntArray) {
+
     for (j in 1 until array.size) {
         val key = array[j]
         var i = j - 1
@@ -18,19 +19,16 @@ fun insertionSort(array: IntArray) {
         }
         array[i + 1] = key
     }
+
 }
 
 fun main() {
+
     val array = intArrayOf(5, 2, 4, 6, 1, 3)
 
-    println("Before InsertionSort")
-    for (a in array) {
-        print("$a ")
-    }
+    println("Before InsertionSort\n ${array.joinToString()}")
 
-    println("\n\nAfter InsertionSort")
     insertionSort(array)
-    for (a in array) {
-        print("$a ")
-    }
+
+    println("\nAfter InsertionSort\n ${array.joinToString()}")
 }
---
+++ src/MergeSort.kt
@@ -0,0 +1,61 @@
+/**
+ * Merge sort
+ *
+ * Сортировка слиянием
+ *
+ * Complexity Analysis:
+ * Time complexity : O(n*log(n)).
+ * Space complexity : O(n).
+ */
+
+private fun mergeSort(array: IntArray, start: Int, end: Int) {
+
+    if (start < end) {
+        val middle = (start + end) / 2
+        mergeSort(array, start, middle)
+        mergeSort(array, middle + 1, end)
+        merge(array, start, middle, end)
+    }
+
+}
+
+private fun merge(array: IntArray, start: Int, middle: Int, end: Int) {
+
+    val indexLeft = middle - start + 1
+    val indexRight = end - middle
+    val leftSubArray = mutableListOf<Int>()
+    val rightSubArray = mutableListOf<Int>()
+
+    for (i in 0 until indexLeft)
+        leftSubArray.add(array[start + i])
+    for (i in 0 until indexRight)
+        rightSubArray.add(array[middle + i + 1])
+
+    leftSubArray.add(Int.MAX_VALUE)
+    rightSubArray.add(Int.MAX_VALUE)
+    var i = 0
+    var j = 0
+
+    for (k in start..end) {
+        if (leftSubArray[i] <= rightSubArray[j]) {
+            array[k] = leftSubArray[i]
+            i++
+        } else {
+            array[k] = rightSubArray[j]
+            j++
+        }
+    }
+
+}
+
+fun main() {
+
+    val array = intArrayOf(5, 8, 2, 4, 7, 6, 1, 3)
+
+    println("Before MergeSort\n ${array.joinToString()}")
+
+    mergeSort(array, 0, array.size - 1)
+
+    println("\nAfter MergeSort\n ${array.joinToString()}")
+
+}

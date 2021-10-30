3b6275a1382e01ef46eb715c4333a481e97b31d3
Zhek <Zhekfake@gmail.com>
2021-10-30 22:46:30 +0300

    added averageSalaryExcludingtheMinimumandMaximumSalary1491

---
+++ src/AverageSalaryExcludingtheMinimumandMaximumSalary1491.kt
@@ -0,0 +1,49 @@
+import kotlin.test.assertEquals
+
+/**
+ * Given an array of unique integers salary where salary[i] is the salary of the employee i.
+ * Return the average salary of employees excluding the minimum and maximum salary.
+ *
+ * Example 1:
+ * Input: salary = [4000,3000,1000,2000]
+ * Output: 2500.00000
+ * Explanation: Minimum salary and maximum salary are 1000 and 4000 respectively.
+ * Average salary excluding minimum and maximum salary is (2000+3000)/2= 2500
+ *
+ * Example 2:
+ * Input: salary = [1000,2000,3000]
+ * Output: 2000.00000
+ * Explanation: Minimum salary and maximum salary are 1000 and 3000 respectively.
+ * Average salary excluding minimum and maximum salary is (2000)/1= 2000
+ *
+ * Example 3:
+ * Input: salary = [6000,5000,4000,3000,2000,1000]
+ * Output: 3500.00000
+ *
+ * Example 4:
+ * Input: salary = [8000,9000,2000,3000,6000,1000]
+ * Output: 4750.00000
+ *
+ * Constraints:
+ * 3 <= salary.length <= 100
+ * 10^3 <= salary[i] <= 10^6
+ * salary[i] is unique.
+ * Answers within 10^-5 of the actual value will be accepted as correct.
+ */
+fun averageSalaryExcludingtheMinimumandMaximumSalary1491(salary: IntArray): Double {
+    val min = salary.minOrNull()
+    val max = salary.maxOrNull()
+
+    return salary.filter { it != min && it != max }.average()
+}
+
+fun main() {
+    val salary1 = intArrayOf(4000, 3000, 1000, 2000)
+    assertEquals(2500.0, averageSalaryExcludingtheMinimumandMaximumSalary1491(salary1), "example1 error")
+    val salary2 = intArrayOf(1000, 2000, 3000)
+    assertEquals(2000.0, averageSalaryExcludingtheMinimumandMaximumSalary1491(salary2), "example2 error")
+    val salary3 = intArrayOf(6000, 5000, 4000, 3000, 2000, 1000)
+    assertEquals(3500.0, averageSalaryExcludingtheMinimumandMaximumSalary1491(salary3), "example3 error")
+    val salary4 = intArrayOf(8000, 9000, 2000, 3000, 6000, 1000)
+    assertEquals(4750.0, averageSalaryExcludingtheMinimumandMaximumSalary1491(salary4), "example4 error")
+}

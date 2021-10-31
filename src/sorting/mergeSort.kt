74c1239b144c8192b1261d1358eca38cbc18c84e
Zhek <Zhekfake@gmail.com>
2021-10-31 22:12:45 +0300

    code cleanup

--- src/sorting/mergeSort.kt
+++ src/sorting/mergeSort.kt
@@ -1,3 +1,5 @@
+package sorting
+
 /**
  * Merge sort
  *
@@ -7,7 +9,6 @@
  * Time complexity : O(n*log(n)).
  * Space complexity : O(n).
  */
-
 private fun mergeSort(array: IntArray, start: Int, end: Int) {
     if (start < end) {
         val middle = (start + end) / 2
@@ -18,14 +19,14 @@ private fun mergeSort(array: IntArray, start: Int, end: Int) {
 }
 
 private fun merge(array: IntArray, start: Int, middle: Int, end: Int) {
-    val indexLeft = middle - start + 1
-    val indexRight = end - middle
+    val leftSize = middle - start + 1
+    val rightSize = end - middle
     val leftSubArray = mutableListOf<Int>()
     val rightSubArray = mutableListOf<Int>()
 
-    for (i in 0 until indexLeft)
+    for (i in 0 until leftSize)
         leftSubArray.add(array[start + i])
-    for (i in 0 until indexRight)
+    for (i in 0 until rightSize)
         rightSubArray.add(array[middle + i + 1])
 
     leftSubArray.add(Int.MAX_VALUE)

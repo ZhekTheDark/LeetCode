20e47d2ccfc450638b06587f0fda4dbf9aad36b9
Zhek <Zhekfake@gmail.com>
2021-11-10 22:58:44 +0300

    code cleanup

--- src/RunningSumOf1dArray.kt
+++ src/RunningSumOf1dArray.kt
@@ -29,19 +29,15 @@
  */
 
 private fun runningSum(nums: IntArray): IntArray {
-
     for (i in 1 until nums.size) {
         nums[i] = nums[i] + nums[i - 1]
     }
 
     return (nums)
-
 }
 
 private fun main() {
-
     val nums = intArrayOf(3, 1, 2, 10, 1)
 
     println(runningSum(nums).joinToString())
-
 }

635ab1bf2366a612c498352b416c5f2023b62625
ZhekTheDark <Zhekfake@gmail.com>
2021-11-30 16:21:05 +0300

    updated FindTheTownJudge997.kt (#3)
    Co-authored-by: Evgeniy Bobrutskov <EVBobrutskov@sberbank.ru>

--- src/com/easy/FindTheTownJudge997.kt
+++ src/com/easy/FindTheTownJudge997.kt
@@ -71,4 +71,8 @@ fun main() {
     val example1N = 2
     val example1Trust = arrayOf(intArrayOf(1, 2))
     assertEquals(2, findJudge(example1N, example1Trust), "FindTheTownJudge997.kt example 1 error")
+
+    val example2N = 3
+    val example2Trust = arrayOf(intArrayOf(1,3), intArrayOf(2,3))
+    assertEquals(3, findJudge(example2N, example2Trust), "FindTheTownJudge997.kt example 2 error")
 }

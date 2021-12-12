6b3c0f00db091ab004ba55213a5ac290b4273e6e
Zhek <Zhekfake@gmail.com>
2021-12-12 21:14:25 +0300

    updated LargestOddNumberШnString1903 added example1

--- src/com/easy/LargestOddNumberШnString1903.kt
+++ src/com/easy/LargestOddNumberШnString1903.kt
@@ -1,5 +1,12 @@
 package com.easy
 
+import kotlin.test.assertEquals
+
 fun largestOddNumber(num: String): String {
     return num.dropLastWhile { Character.getNumericValue(it) % 2 == 0 }
+}
+
+fun main() {
+    val example1Num = "52"
+    assertEquals("5", largestOddNumber(example1Num), "LargestOddNumberШnString1903 example1 error")
 }

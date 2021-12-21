7261604c1dce105e5d3084011e6d356f606bf6fb
Zhek <Zhekfake@gmail.com>
2021-12-21 23:31:58 +0300

    MinimumDifferenceBetweenHighestAndLowestOfKScores1984 added example1

--- src/com/easy/MinimumDifferenceBetweenHighestAndLowestOfKScores1984.kt
+++ src/com/easy/MinimumDifferenceBetweenHighestAndLowestOfKScores1984.kt
@@ -1,2 +1,13 @@
 package com.easy
 
+import kotlin.test.assertEquals
+
+fun minimumDifference(nums: IntArray, k: Int): Int {
+    return 0
+}
+
+fun main() {
+    val example1Nums = intArrayOf(90)
+    val example1K = 1
+    assertEquals(0, minimumDifference(example1Nums, example1K), "MinimumDifferenceBetweenHighestAndLowestOfKScores1984 example1 error")
+}

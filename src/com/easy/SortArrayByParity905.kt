ae861fbf97cb05708404bbb6ed09bd80d9940dcd
Zhek <Zhekfake@gmail.com>
2022-02-01 22:02:31 +0300

    SortArrayByParity905 added example1

--- src/com/easy/SortArrayByParity905.kt
+++ src/com/easy/SortArrayByParity905.kt
@@ -1,5 +1,13 @@
 package com.easy
 
+import kotlin.test.assertEquals
+
 fun sortArrayByParity(nums: IntArray): IntArray {
     return intArrayOf()
+}
+
+fun main() {
+    val example1Nums = intArrayOf(3,1,2,4)
+    val example1Res = intArrayOf(2,4,3,1)
+    assertEquals(example1Res, sortArrayByParity(example1Nums), "sortArrayByParity example1 error")
 }

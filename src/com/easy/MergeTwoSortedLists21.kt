ac9d239e4a5d21a21cd4ab95005cfc64965874e9
Zhek <Zhekfake@gmail.com>
2023-01-22 20:47:06 +0300

    MergeTwoSortedLists21 added example1 cleanup

--- src/com/easy/MergeTwoSortedLists21.kt
+++ src/com/easy/MergeTwoSortedLists21.kt
@@ -20,19 +20,35 @@ fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
 }
 
 fun main() {
-    Input: list1 = [1,2,4], list2 = [1,3,4]
-    Output: [1,1,2,3,4,4]
-
     val node13 = ListNode(4).apply { next = null }
     val node12 = ListNode(2).apply { next = node13 }
     val node11 = ListNode(1).apply { next = node12 }
 
     val node23 = ListNode(4).apply { next = null }
-    val node22 = ListNode(2).apply { next = node23 }
+    val node22 = ListNode(3).apply { next = node23 }
     val node21 = ListNode(1).apply { next = node22 }
 
+    val expectedListNode6 = ListNode(4).apply { next = null }
+    val expectedListNode5 = ListNode(4).apply { next = expectedListNode6 }
+    val expectedListNode4 = ListNode(3).apply { next = expectedListNode5 }
+    val expectedListNode3 = ListNode(2).apply { next = expectedListNode4 }
+    val expectedListNode2 = ListNode(1).apply { next = expectedListNode3 }
+    val expectedListNode1 = ListNode(1).apply { next = expectedListNode2 }
+    val actualListNode = mergeTwoLists(node11, node12)
+    assertEquals(expectedListNode1, actualListNode)
+}
 
-    val expectedListNode6 = ListNode(1).apply { next = null }
-    val expectedListNode =
-    assertEquals()
+fun assertEquals(expected: ListNode, actual: ListNode): Boolean {
+    var tempExpected: ListNode? = expected
+    var tempActual: ListNode? = actual
+    do {
+        if (tempExpected?.`val` != tempActual?.`val`) {
+            return false
+        } else {
+            tempExpected = tempExpected?.next
+            tempActual = tempActual?.next
+        }
+    } while (expected.next != null)
+
+    return true
 }

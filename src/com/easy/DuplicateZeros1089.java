952c0ec1aabcb2c520e93f514571d3755afe1b00
Zhek <Zhekfake@gmail.com>
2023-02-05 20:38:51 +0300

    added DuplicateZeros1089

---
+++ src/com/easy/DuplicateZeros1089.java
@@ -0,0 +1,71 @@
+package com.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+ * Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+ * <p>
+ * Example 1:
+ * Input: arr = [1,0,2,3,0,4,5,0]
+ * Output: [1,0,0,2,3,0,0,4]
+ * Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+ * <p>
+ * Example 2:
+ * Input: arr = [1,2,3]
+ * Output: [1,2,3]
+ * Explanation: After calling your function, the input array is modified to: [1,2,3]
+ * <p>
+ * Constraints:
+ * 1 <= arr.length <= 10^4
+ * 0 <= arr[i] <= 9
+ */
+public class DuplicateZeros1089 {
+
+    /**
+     * Complexity Analysis:
+     * Time complexity : O(n).
+     * Space complexity : O(n).
+     * <p>
+     * Runtime: 2 ms, faster than 70.8% of Java online submissions for Squares of a Duplicate Zeros.
+     * Memory Usage: 43 MB, less than 37.6% of Java online submissions for Squares of a Duplicate Zeros.
+     */
+    public void duplicateZerosExtraSpace(int[] arr) {
+        List<Integer> res = new ArrayList<>();
+
+        for (int element : arr) {
+            if (element == 0) {
+                res.add(element);
+                res.add(element);
+            } else {
+                res.add(element);
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = res.get(i);
+        }
+    }
+
+//    1,0,2,3,0,0,4,5
+//    1,0,0,2,3,0,0,0
+    public void duplicateZeros(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            if (arr[left] == 0) {
+                if (left == right) {
+                    right--;
+                    arr[arr.length - 1] = 0;
+                }
+                right--;
+                left++;
+            }
+            left++;
+        }
+
+
+    }
+}

9e4caaf84e6ea333ff9245e5b87c295ecfb86a9b
Zhek <Zhekfake@gmail.com>
2023-02-09 21:18:41 +0300

    RansomNote383 code cleanup

--- src/com/easy/RansomNote383.java
+++ src/com/easy/RansomNote383.java
@@ -6,19 +6,19 @@ import java.util.Map;
 /**
  * Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
  * Each letter in magazine can only be used once in ransomNote.
- *
+ * <p>
  * Example 1:
  * Input: ransomNote = "a", magazine = "b"
  * Output: false
- *
+ * <p>
  * Example 2:
  * Input: ransomNote = "aa", magazine = "ab"
  * Output: false
- *
+ * <p>
  * Example 3:
  * Input: ransomNote = "aa", magazine = "aab"
  * Output: true
- *
+ * <p>
  * Constraints:
  * 1 <= ransomNote.length, magazine.length <= 10^5
  * ransomNote and magazine consist of lowercase English letters.
@@ -56,7 +56,6 @@ public class RansomNote383 {
 
         return true;
     }
-
 
     /**
      * Complexity Analysis:

af506c2675d48de70247b07142021a556df83210
Zhek <Zhekfake@gmail.com>
2023-03-02 22:18:45 +0300

    implemented more ThirdMaximumNumber414

--- src/com/easy/ThirdMaximumNumber414.java
+++ src/com/easy/ThirdMaximumNumber414.java
@@ -75,4 +75,70 @@ public class ThirdMaximumNumber414 {
 
         return thirdMax.getKey();
     }
+
+    /**
+     * Complexity Analysis:
+     * Time complexity : O(n).
+     * Space complexity : O(1).
+     * <p>
+     * Runtime: 4 ms, faster than 46.98% of Java online submissions for Third Maximum Number.
+     * Memory Usage: 41.9 MB, less than 74.68% of Java online submissions for Third Maximum Number.
+     */
+    public int thirdMax1(int[] nums) {
+        Integer firstMax = null;
+        Integer secondMax = null;
+        Integer thirdMax = null;
+
+        for (Integer num : nums) {
+            if ( num.equals(firstMax) || num.equals(secondMax) || num.equals(thirdMax)) {
+                continue;
+            }
+
+            if (firstMax == null || firstMax <= num) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (secondMax == null || secondMax <= num) {
+                thirdMax = secondMax;
+                secondMax = num;
+            } else if (thirdMax == null || thirdMax <= num) {
+                thirdMax = num;
+            }
+        }
+
+        if (thirdMax == null) {
+            return firstMax;
+        }
+
+        return thirdMax;
+    }
+
+    /**
+     * Complexity Analysis:
+     * Time complexity : O(n).
+     * Space complexity : O(1).
+     * <p>
+     * Runtime: 0 ms, faster than 100% of Java online submissions for Third Maximum Number.
+     * Memory Usage: 42.3 MB, less than 35.87% of Java online submissions for Third Maximum Number.
+     */
+    public int thirdMax2(int[] nums) {
+        long firstMax = Long.MIN_VALUE;
+        long secondMax = Long.MIN_VALUE;
+        long thirdMax = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num > firstMax) {
+                thirdMax = secondMax;
+                secondMax = firstMax;
+                firstMax = num;
+            } else if (num > secondMax && num != firstMax) {
+                thirdMax = secondMax;
+                secondMax = num;
+            } else if (num > thirdMax && num != secondMax && num != firstMax) {
+                thirdMax = num;
+            }
+        }
+
+        return thirdMax == Long.MIN_VALUE ? (int) firstMax : (int) thirdMax;
+    }
 }

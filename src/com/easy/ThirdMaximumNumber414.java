9cca5900c803a50274d793818688805e17af5246
Zhek <Zhekfake@gmail.com>
2023-03-03 20:56:24 +0300

    code cleanup

--- src/com/easy/ThirdMaximumNumber414.java
+++ src/com/easy/ThirdMaximumNumber414.java
@@ -4,7 +4,7 @@ import javafx.util.Pair;
 
 /**
  * Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
- *
+ * <p>
  * Example 1:
  * Input: nums = [3,2,1]
  * Output: 1
@@ -12,7 +12,7 @@ import javafx.util.Pair;
  * The first distinct maximum is 3.
  * The second distinct maximum is 2.
  * The third distinct maximum is 1.
- *
+ * <p>
  * Example 2:
  * Input: nums = [1,2]
  * Output: 2
@@ -20,7 +20,7 @@ import javafx.util.Pair;
  * The first distinct maximum is 2.
  * The second distinct maximum is 1.
  * The third distinct maximum does not exist, so the maximum (2) is returned instead.
- *
+ * <p>
  * Example 3:
  * Input: nums = [2,2,3,1]
  * Output: 1
@@ -28,11 +28,11 @@ import javafx.util.Pair;
  * The first distinct maximum is 3.
  * The second distinct maximum is 2 (both 2's are counted together since they have the same value).
  * The third distinct maximum is 1.
- *
+ * <p>
  * Constraints:
  * 1 <= nums.length <= 10^4
  * -2^31 <= nums[i] <= 2^31 - 1
- *
+ * <p>
  * Follow up: Can you find an O(n) solution?
  */
 public class ThirdMaximumNumber414 {
@@ -90,7 +90,7 @@ public class ThirdMaximumNumber414 {
         Integer thirdMax = null;
 
         for (Integer num : nums) {
-            if ( num.equals(firstMax) || num.equals(secondMax) || num.equals(thirdMax)) {
+            if (num.equals(firstMax) || num.equals(secondMax) || num.equals(thirdMax)) {
                 continue;
             }
 

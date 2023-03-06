6ba104292d523070d61f2a50a15d1f190e8ceb38
Zhek <Zhekfake@gmail.com>
2023-03-06 21:07:19 +0300

    FindAllNumbersDisappearedInAnArray448 added description

--- src/com/easy/FindAllNumbersDisappearedInAnArray448.java
+++ src/com/easy/FindAllNumbersDisappearedInAnArray448.java
@@ -3,6 +3,24 @@ package com.easy;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+ * <p>
+ * Example 1:
+ * Input: nums = [4,3,2,7,8,2,3,1]
+ * Output: [5,6]
+ * <p>
+ * Example 2:
+ * Input: nums = [1,1]
+ * Output: [2]
+ * <p>
+ * Constraints:
+ * n == nums.length
+ * 1 <= n <= 10^5
+ * 1 <= nums[i] <= n
+ * <p>
+ * Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+ */
 public class FindAllNumbersDisappearedInAnArray448 {
 
     /**

5d929c48bc05bfe81212aff79951820bcba0169f
Zhek <Zhekfake@gmail.com>
2023-03-24 23:17:49 +0300

    SortArrayByParity905 code cleanup

--- src/com/easy/SortArrayByParity905.kt
+++ src/com/easy/SortArrayByParity905.kt
@@ -44,8 +44,8 @@ fun sortArrayByParity(nums: IntArray): IntArray {
 }
 
 fun main() {
-    val example1Nums = intArrayOf(3,1,2,4)
-    val example1Res = intArrayOf(2,4,3,1)
+    val example1Nums = intArrayOf(3, 1, 2, 4)
+    val example1Res = intArrayOf(2, 4, 3, 1)
     assertEquals(example1Res, sortArrayByParity(example1Nums), "sortArrayByParity example1 error")
 
     val example2Nums = intArrayOf(0)

38d2bef3813270427356235170733050f0fbd5fd
Zhek <Zhekfake@gmail.com>
2023-03-29 21:36:49 +0300

    CanPlaceFlowers605 code cleanup

--- src/com/easy/CanPlaceFlowers605.java
+++ src/com/easy/CanPlaceFlowers605.java
@@ -3,18 +3,18 @@ package com.easy;
 /**
  * You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be
  * planted in adjacent plots.
- *
+ * <p>
  * Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n,
  * return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
- *
+ * <p>
  * Example 1:
  * Input: flowerbed = [1,0,0,0,1], n = 1
  * Output: true
- *
+ * <p>
  * Example 2:
  * Input: flowerbed = [1,0,0,0,1], n = 2
  * Output: false
- *
+ * <p>
  * Constraints:
  * 1 <= flowerbed.length <= 2 * 10^4
  * flowerbed[i] is 0 or 1.
@@ -27,7 +27,7 @@ public class CanPlaceFlowers605 {
      * Complexity Analysis:
      * Time complexity : O(n).
      * Space complexity : O(1).
-     *
+     * <p>
      * Runtime: 1 ms, faster than 89.19% of Java online submissions for Can Place Flowers.
      * Memory Usage: 40.3 MB, less than 91.90% of Java online submissions for Can Place Flowers.
      */

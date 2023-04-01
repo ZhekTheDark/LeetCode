ffdcd6626d5d131471387a44e8ab8fb1ee47111a
Zhek <Zhekfake@gmail.com>
2023-04-01 22:50:33 +0300

    MinStack155 working on

--- src/com/medium/MinStack155.java
+++ src/com/medium/MinStack155.java
@@ -1,4 +1,27 @@
 package com.medium;
 
 public class MinStack155 {
+
+    class MinStack {
+
+        public MinStack() {
+
+        }
+
+        public void push(int val) {
+
+        }
+
+        public void pop() {
+
+        }
+
+        public int top() {
+
+        }
+
+        public int getMin() {
+
+        }
+    }
 }

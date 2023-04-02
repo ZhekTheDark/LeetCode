8d7d3b63f61c18d8707aeac96142a58e13737a2a
Zhek <Zhekfake@gmail.com>
2023-04-02 23:25:22 +0300

    MinStack155 implemented

--- src/com/medium/MinStack155.java
+++ src/com/medium/MinStack155.java
@@ -1,27 +1,33 @@
 package com.medium;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MinStack155 {
 
     class MinStack {
 
-        public MinStack() {
+        List<Integer> stack;
 
+        public MinStack() {
+            stack = new ArrayList<>();
         }
 
         public void push(int val) {
-
+            stack.add(val);
         }
 
         public void pop() {
-
+            stack.remove(stack.size() - 1);
         }
 
         public int top() {
-
+            return stack.get(stack.size() - 1);
         }
 
         public int getMin() {
-
+            return Collections.min(stack);
         }
     }
 }

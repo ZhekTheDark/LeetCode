b4516610a39106dc4aed068b93439401784dcad6
Zhek <Zhekfake@gmail.com>
2023-04-13 21:42:39 +0300

    SquaresOfASortedArray977 code cleanup

--- src/com/easy/SquaresOfASortedArray977.java
+++ src/com/easy/SquaresOfASortedArray977.java
@@ -1,7 +1,5 @@
 package com.easy;
 
-import java.util.*;
-
 /**
  * Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
  * <p>

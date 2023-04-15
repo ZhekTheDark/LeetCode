524f252bdb51294f70bcb9a41b11e0927420e8e8
Zhek <Zhekfake@gmail.com>
2023-04-15 16:38:52 +0300

    MinimumDifferenceBetweenHighestAndLowestOfKScores1984 code cleanup

--- src/com/easy/MinimumDifferenceBetweenHighestAndLowestOfKScores1984.kt
+++ src/com/easy/MinimumDifferenceBetweenHighestAndLowestOfKScores1984.kt
@@ -51,9 +51,17 @@ fun minimumDifference(nums: IntArray, k: Int): Int {
 fun main() {
     val example1Nums = intArrayOf(90)
     val example1K = 1
-    assertEquals(0, minimumDifference(example1Nums, example1K), "MinimumDifferenceBetweenHighestAndLowestOfKScores1984 example1 error")
+    assertEquals(
+        0,
+        minimumDifference(example1Nums, example1K),
+        "MinimumDifferenceBetweenHighestAndLowestOfKScores1984 example1 error"
+    )
 
-    val example2Nums = intArrayOf(9,4,1,7)
+    val example2Nums = intArrayOf(9, 4, 1, 7)
     val example2K = 1
-    assertEquals(0, minimumDifference(example2Nums, example2K), "MinimumDifferenceBetweenHighestAndLowestOfKScores1984 example1 error")
+    assertEquals(
+        0,
+        minimumDifference(example2Nums, example2K),
+        "MinimumDifferenceBetweenHighestAndLowestOfKScores1984 example1 error"
+    )
 }

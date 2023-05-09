942ad5e5c2cc71b3c8d31aa2cca93cf7e4cc27b9
Zhek <Zhekfake@gmail.com>
2023-05-09 21:23:16 +0300

    _739_DailyTemperatures init

---
+++ src/com/medium/_739_DailyTemperatures.java
@@ -0,0 +1,23 @@
+package com.medium;
+
+import java.util.Stack;
+
+public class _739_DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int m = temperatures.length;
+        int[] res = new int[m];
+//        Stack<Integer> stack = new Stack<>();
+//
+//        for (int i = 0; i < m; i++) {
+//            while (!stack.empty() &&
+//                    temperatures[stack.peek()] < temperatures[i]
+//            ) {
+//                res[stack.peek()] = i - stack.pop();
+//            }
+//            stack.push(i);
+//        }
+
+        return res;
+    }
+}

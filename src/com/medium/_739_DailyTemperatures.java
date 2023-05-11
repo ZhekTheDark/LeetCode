16b1409077828bc9db9f3983f14b7ea5ac38ca65
Zhek <Zhekfake@gmail.com>
2023-05-11 21:13:02 +0300

    _739_DailyTemperatures implemented

--- src/com/medium/_739_DailyTemperatures.java
+++ src/com/medium/_739_DailyTemperatures.java
@@ -26,18 +26,18 @@ import java.util.Stack;
 public class _739_DailyTemperatures {
 
     public int[] dailyTemperatures(int[] temperatures) {
-        int m = temperatures.length;
-        int[] res = new int[m];
-//        Stack<Integer> stack = new Stack<>();
-//
-//        for (int i = 0; i < m; i++) {
-//            while (!stack.empty() &&
-//                    temperatures[stack.peek()] < temperatures[i]
-//            ) {
-//                res[stack.peek()] = i - stack.pop();
-//            }
-//            stack.push(i);
-//        }
+        int n = temperatures.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.empty() &&
+                    temperatures[stack.peek()] < temperatures[i]
+            ) {
+                res[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
 
         return res;
     }

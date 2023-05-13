118d01ab71a3dd28923cb9b8ccbd157c7153f24e
Zhek <Zhekfake@gmail.com>
2023-05-13 15:36:56 +0300

    _739_DailyTemperatures code cleanup

--- src/com/medium/_739_DailyTemperatures.java
+++ src/com/medium/_739_DailyTemperatures.java
@@ -6,19 +6,19 @@ import java.util.Stack;
  * Given an array of integers temperatures represents the daily temperatures, return an array answer such that
  * answer[i] is the number of days you have to wait after the ith day to get a warmer temperature.
  * If there is no future day for which this is possible, keep answer[i] == 0 instead.
- *
+ * <p>
  * Example 1:
  * Input: temperatures = [73,74,75,71,69,72,76,73]
  * Output: [1,1,4,2,1,1,0,0]
- *
+ * <p>
  * Example 2:
  * Input: temperatures = [30,40,50,60]
  * Output: [1,1,1,0]
- *
+ * <p>
  * Example 3:
  * Input: temperatures = [30,60,90]
  * Output: [1,1,0]
- *
+ * <p>
  * Constraints:
  * 1 <= temperatures.length <= 10^5
  * 30 <= temperatures[i] <= 100
@@ -29,7 +29,7 @@ public class _739_DailyTemperatures {
      * Complexity Analysis:
      * Time complexity : O(n).
      * Space complexity : O(n).
-     *
+     * <p>
      * Runtime: 176 ms, faster than 38.73% of Java online submissions for 739 Daily Temperatures.
      * Memory Usage: 54.7 MB, less than 73.96% of Java online submissions for 739 Daily Temperatures.
      */

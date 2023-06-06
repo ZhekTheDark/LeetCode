b05997cd4e673132600cb3b8f3b0ecc4ca9416a8
Zhek <Zhekfake@gmail.com>
2023-06-06 22:33:30 +0300

    _901_OnlineStockSpan done

--- src/com/medium/_901_OnlineStockSpan.kt
+++ src/com/medium/_901_OnlineStockSpan.kt
@@ -1,8 +1,17 @@
 package com.medium
 
+import java.util.*
+
 class _901_OnlineStockSpan {
 
-    fun next(price: Int): Int {
+    private val stack = Stack<Pair<Int, Int>>()
 
+    fun next(price: Int): Int {
+        var days = 1
+        while (stack.isNotEmpty() && (stack.peek().first <= price)) {
+            days += stack.pop().second
+        }
+        stack.push(price to days)
+        return days
     }
 }

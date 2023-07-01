6213ee23d0048e32aaa5b63691bafc078c9266fb
Zhek <Zhekfake@gmail.com>
2023-07-01 21:26:30 +0300

    _13_RomanToInteger done

--- src/com/easy/_13_RomanToInteger.kt
+++ src/com/easy/_13_RomanToInteger.kt
@@ -2,42 +2,49 @@ package com.easy
 
 class _13_RomanToInteger {
 
+    private val map = mapOf(
+        'I' to 1,
+        'V' to 5,
+        'X' to 10,
+        'L' to 50,
+        'C' to 100,
+        'D' to 500,
+        'M' to 1000,
+    )
+    private val subtraction = mapOf(
+        'V' to 'I',
+        'X' to 'I',
+        'L' to 'X',
+        'C' to 'X',
+        'D' to 'C',
+        'M' to 'C',
+    )
+
     fun romanToInt(s: String): Int {
         var res = 0
-        val map = mapOf<Char, Int>(
-            'I' to 1,
-            'V' to 5,
-            'X' to 10,
-            'L' to 50,
-            'C' to 100,
-            'D' to 500,
-            'M' to 1000,
-        )
-        val subtraction = mapOf(
-            'V' to 'I',
-            'X' to 'I',
-            'L' to 'X',
-            'C' to 'X',
-            'D' to 'C',
-            'M' to 'C',
-        )
-
         var i = 0
         while (i < s.length) {
-            val next: Int? = if (i < s.length - 1) {
+            val next: Char? = if (i < s.length - 1) {
                 s[i + 1]
-                i++
             } else {
                 null
             }
-            res += eval(map[s[i]]!!, map.get(next))
+            res += eval(s[i], next)
+            if (subtraction[next] == s[i]) i++
             i++
         }
-
         return res
     }
 
-    fun eval(first: Int, second: Int?): Int {
-
+    private fun eval(first: Char, second: Char?): Int {
+        return if (subtraction[second] == first) {
+            map[second]!!.minus(map[first]!!)
+        } else {
+            map[first]!!
+        }
     }
+}
+
+fun main() {
+    println(_13_RomanToInteger().romanToInt("LVIII"))
 }

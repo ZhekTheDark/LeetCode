35d64f3d029ce63d4b78ac1d3de6a453df8a3647
Zhek <Zhekfake@gmail.com>
2023-07-10 20:28:53 +0300

    _21_MergeTwoSortedLists code cleanup

--- src/com/easy/_21_MergeTwoSortedLists.kt
+++ src/com/easy/_21_MergeTwoSortedLists.kt
@@ -1,5 +1,29 @@
 package com.easy
 
+/**
+ * You are given the heads of two sorted linked lists list1 and list2.
+ *
+ * Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ *
+ * Return the head of the merged linked list.
+ *
+ * Example 1:
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ *
+ * Example 2:
+ * Input: list1 = [], list2 = []
+ * Output: []
+ *
+ * Example 3:
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ *
+ * Constraints:
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ */
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
@@ -86,22 +110,6 @@ fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
             }
         }
     }
-//    if (list1 == null && list2 == null) {
-//        return null
-//    }
-//    if (list1 == null) {
-//        return list2
-//    }
-//    if (list2 == null) {
-//        return list1
-//    }
-//    return if (list1.`val` < list2.`val`) {
-//        list1.next = mergeTwoLists(list1.next, list2)
-//        list1
-//    } else {
-//        list2.next = mergeTwoLists(list1, list2.next)
-//        list2
-//    }
 }
 
 fun main() {

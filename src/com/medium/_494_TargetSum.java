cbc96c76bf386db6d914f8d19d8e263615480a5d
Zhek <Zhekfake@gmail.com>
2023-07-13 21:38:18 +0300

    _494_TargetSum added

---
+++ src/com/medium/_494_TargetSum.java
@@ -0,0 +1,11 @@
+package com.medium;
+
+import java.util.Stack;
+
+public class _494_TargetSum {
+
+    public int findTargetSumWays(int[] nums, int target) {
+        Stack<Integer> stack = new Stack<>();
+        
+    }
+}

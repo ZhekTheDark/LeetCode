7e05ee93de96a0bc08d86d68ec9ff7b20790a317
Zhek <Zhekfake@gmail.com>
2023-07-14 19:08:31 +0300

    _23_MergeKSortedLists working

--- src/com/hard/_23_MergeKSortedLists.java
+++ src/com/hard/_23_MergeKSortedLists.java
@@ -39,4 +39,24 @@ public class _23_MergeKSortedLists {
         }
         return res;
     }
+
+    private static ListNode toListNodes(int[] arr) {
+        if (arr.length == 0) return null;
+        ListNode node = new ListNode(arr[0]);
+        node.next = toListNodes(Arrays.copyOfRange(arr, 1, arr.length));
+        return node;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 4, 5};
+        int[] arr2 = {1, 3, 4};
+        int[] arr3 = {2, 6};
+        ListNode node1 = toListNodes(arr1);
+        ListNode node2 = toListNodes(arr2);
+        ListNode node3 = toListNodes(arr3);
+        ListNode[] input = {node1, node2, node3};
+        ListNode res = mergeKLists(input);
+
+        System.out.println("GG WP");
+    }
 }

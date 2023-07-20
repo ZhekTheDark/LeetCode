ed15186d6be1db78987e31c0a7661e313acda631
Zhek <Zhekfake@gmail.com>
2023-07-20 22:19:35 +0300

    _3_LongestSubstringWithoutRepeatingCharacters added description

--- src/com/medium/_3_LongestSubstringWithoutRepeatingCharacters.java
+++ src/com/medium/_3_LongestSubstringWithoutRepeatingCharacters.java
@@ -5,8 +5,35 @@ import kotlin.test.AssertionsKt;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Given a string s, find the length of the longest substring without repeating characters.
+ *
+ * Example 1:
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ *
+ * Example 2:
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ *
+ * Example 3:
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ *
+ * Constraints:
+ * 0 <= s.length <= 5 * 10^4
+ * s consists of English letters, digits, symbols and spaces.
+ */
 public class _3_LongestSubstringWithoutRepeatingCharacters {
 
+    /**
+     * Runtime: 8ms Beats 60.84% of users with Java
+     * Memory: 43.91mb Beats 36.66% of users with Java
+     */
     public static int lengthOfLongestSubstring(String s) {
         int res = 0;
         Set<Character> set = new HashSet<>();

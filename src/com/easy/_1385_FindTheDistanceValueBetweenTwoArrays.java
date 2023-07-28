0a4549c8ad2522100349bab7323a1c45fccae32e
Zhek <Zhekfake@gmail.com>
2023-07-28 21:07:35 +0300

    _1385_FindTheDistanceValueBetweenTwoArrays added

---
+++ src/com/easy/_1385_FindTheDistanceValueBetweenTwoArrays.java
@@ -0,0 +1,10 @@
+package com.easy;
+
+public class _1385_FindTheDistanceValueBetweenTwoArrays {
+
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int res = 0;
+
+        return res;
+    }
+}

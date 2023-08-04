677798f8dda368b98318b7191b84eab6acba1b4e
Zhek <Zhekfake@gmail.com>
2023-08-04 22:52:49 +0300

    _394_DecodeString.java working

--- src/com/medium/_394_DecodeString.java
+++ src/com/medium/_394_DecodeString.java
@@ -46,6 +46,42 @@ public class _394_DecodeString {
 //        return res.toString();
 //    }
 
+    public static String decodeString(String s) {
+        Stack<String> substr = new Stack<>();
+        Stack<Integer> repeat = new Stack<>();
+        int index = 0;
+        StringBuilder res = new StringBuilder();
+
+        while (index < s.length()) {
+            if (Character.isDigit(s.charAt(index))) {
+                int i = index;
+                while (Character.isDigit(s.charAt(i))) {
+                    i++;
+                }
+                repeat.push(Integer.parseInt(s.substring(index, i)));
+                index = i;
+            } else if (s.charAt(index) == '[') {
+                substr.push(res.toString());
+                res = new StringBuilder();
+                index++;
+            } else if (s.charAt(index) == ']') {
+                String str = substr.pop();
+                int count = repeat.pop();
+                StringBuilder temp = new StringBuilder(str);
+                for (int i = 0; i < count; i++) {
+                    temp.append(str);
+                }
+                res = new StringBuilder(temp.toString());
+                index++;
+            } else {
+                res.append(s.charAt(index));
+                index++;
+            }
+        }
+
+        return res.toString();
+    }
+
     public static void main(String[] args) {
 //        System.out.println(decodeString("3[a]2[bc]")); //aaabcbc
         System.out.println(decodeString("3[a2[c]]"));

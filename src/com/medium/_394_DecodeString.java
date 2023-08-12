60f9501231eeeacc6a0a482719eaf44ce9269f1e
Zhek <Zhekfake@gmail.com>
2023-08-12 23:05:55 +0300

    _394_DecodeString.java working

--- src/com/medium/_394_DecodeString.java
+++ src/com/medium/_394_DecodeString.java
@@ -4,28 +4,28 @@ import java.util.Stack;
 
 /**
  * Given an encoded string, return its decoded string.
- *
+ * <p>
  * The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated
  * exactly k times. Note that k is guaranteed to be a positive integer.
- *
+ * <p>
  * You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed,
  * etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those
  * repeat numbers, k. For example, there will not be input like 3a or 2[4].
- *
+ * <p>
  * The test cases are generated so that the length of the output will never exceed 105.
- *
+ * <p>
  * Example 1:
  * Input: s = "3[a]2[bc]"
  * Output: "aaabcbc"
- *
+ * <p>
  * Example 2:
  * Input: s = "3[a2[c]]"
  * Output: "accaccacc"
- *
+ * <p>
  * Example 3:
  * Input: s = "2[abc]3[cd]ef"
  * Output: "abcabccdcdcdef"
- *
+ * <p>
  * Constraints:
  * 1 <= s.length <= 30
  * s consists of lowercase English letters, digits, and square brackets '[]'.
@@ -74,9 +74,33 @@ public class _394_DecodeString {
         return res.toString();
     }
 
-//    public static String decodeStringRecursive(String s) {
-//
-//    }
+    int i = 0;
+    public String decodeStringRecursive(String s) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        String tmp_string = "";
+
+        while (i < s.length()) {
+            char c = s.charAt(i);
+            i++;
+
+            if (c == '[') {
+                tmp_string = decodeStringRecursive(s);
+                for (int j = 0; j < count; j++) {
+                    sb.append(tmp_string);
+                }
+                count = 0;
+            } else if (c == ']') {
+                break;
+            } else if (Character.isAlphabetic(c)) {
+                sb.append(c);
+            } else {
+                count = count * 10 + c - '0';
+            }
+        }
+
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
         System.out.println(decodeStringStack("3[a]2[bc]"));  //aaabcbc

a9a3856c4b3883016fe35f2f23e652b17efeccee
Zhek <Zhekfake@gmail.com>
2023-08-17 23:30:35 +0300

    _542_01Matrix working

--- src/com/medium/_542_01Matrix.java
+++ src/com/medium/_542_01Matrix.java
@@ -1,4 +1,17 @@
 package com.medium;
 
 public class _542_01Matrix {
+
+    public int[][] updateMatrix(int[][] mat) {
+//        for (int x  = 0; x <= mat.length; x++) {
+//
+//            for (int y = 0; y <= mat[x].length; y++) {
+//
+//                if (mat[x][y] == 0) continue;
+//
+//            }
+//        }
+
+        return mat;
+    }
 }

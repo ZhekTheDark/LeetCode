b002c6fee6b8f331f346059e85e73c9c7a856424
Zhek <Zhekfake@gmail.com>
2023-08-23 20:54:50 +0300

    _841_KeysAndRooms done

--- src/com/medium/_841_KeysAndRooms.java
+++ src/com/medium/_841_KeysAndRooms.java
@@ -1,4 +1,31 @@
 package com.medium;
 
+import java.util.*;
+
 public class _841_KeysAndRooms {
+
+    public static boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        Set<Integer> visitedRooms = new HashSet<>();
+        Queue<Integer> roomsToOpen = new LinkedList<>(rooms.get(0));
+        visitedRooms.add(0);
+        while (!roomsToOpen.isEmpty()) {
+            Integer roomToOpen = roomsToOpen.poll();
+            if (!visitedRooms.contains(roomToOpen)) {
+                roomsToOpen.addAll(rooms.get(roomToOpen));
+            }
+            visitedRooms.add(roomToOpen);
+        }
+
+        return visitedRooms.size() == rooms.size();
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> rooms = Arrays.asList(
+                Arrays.asList(1),
+                Arrays.asList(2),
+                Arrays.asList(3),
+                Collections.emptyList()
+        );
+        System.out.println(canVisitAllRooms(rooms));
+    }
 }

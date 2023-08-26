5be36c4ea2fb8f7da3af0d6a0c940c6fbdf34729
Zhek <Zhekfake@gmail.com>
2023-08-26 22:01:23 +0300

    _232_ImplementQueueUsingStacks.java added

---
+++ src/com/easy/_232_ImplementQueueUsingStacks.java
@@ -0,0 +1,27 @@
+package com.easy;
+
+public class _232_ImplementQueueUsingStacks {
+
+    class MyQueue {
+
+        public MyQueue() {
+
+        }
+
+        public void push(int x) {
+
+        }
+
+        public int pop() {
+
+        }
+
+        public int peek() {
+
+        }
+
+        public boolean empty() {
+
+        }
+    }
+}

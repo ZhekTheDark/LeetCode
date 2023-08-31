490438fc33febc632d73a133600c26586a9c5a48
Zhek <Zhekfake@gmail.com>
2023-08-31 22:03:57 +0300

    _232_ImplementQueueUsingStacks code cleanup

--- src/com/easy/_232_ImplementQueueUsingStacks.java
+++ src/com/easy/_232_ImplementQueueUsingStacks.java
@@ -1,31 +1,30 @@
 package com.easy;
 
-import java.util.EmptyStackException;
 import java.util.Stack;
 
 /**
  * Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all
  * the functions of a normal queue (push, peek, pop, and empty).
- *
+ * <p>
  * Implement the MyQueue class:
- *
+ * <p>
  * void push(int x) Pushes element x to the back of the queue.
  * int pop() Removes the element from the front of the queue and returns it.
  * int peek() Returns the element at the front of the queue.
  * boolean empty() Returns true if the queue is empty, false otherwise.
  * Notes:
- *
+ * <p>
  * You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
  * Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque
  * (double-ended queue) as long as you use only a stack's standard operations.
- *
+ * <p>
  * Example 1:
  * Input
  * ["MyQueue", "push", "push", "peek", "pop", "empty"]
  * [[], [1], [2], [], [], []]
  * Output
  * [null, null, null, 1, 1, false]
- *
+ * <p>
  * Explanation
  * MyQueue myQueue = new MyQueue();
  * myQueue.push(1); // queue is: [1]
@@ -33,26 +32,20 @@ import java.util.Stack;
  * myQueue.peek(); // return 1
  * myQueue.pop(); // return 1, queue is [2]
  * myQueue.empty(); // return false
- *
+ * <p>
  * Constraints:
  * 1 <= x <= 9
  * At most 100 calls will be made to push, pop, peek, and empty.
  * All the calls to pop and peek are valid.
- *
+ * <p>
  * Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? In other words,
  * performing n operations will take overall O(n) time even if one of those operations may take longer.
  */
 public class _232_ImplementQueueUsingStacks {
 
     /**
-     * Runtime
-     * Details
-     * 0ms
-     * Beats 100.00%of users with Java
-     * Memory
-     * Details
-     * 40.50MB
-     * Beats 21.39%of users with Java
+     * Runtime: 0ms Beats 100.00% of users with Java
+     * Memory: 40.50MB Beats 21.39% of users with Java
      */
     class MyQueue {
 
@@ -69,12 +62,8 @@ public class _232_ImplementQueueUsingStacks {
 
         public int pop() {
             if (queue.empty()) {
-                if (stack.empty()) {
-                    throw new EmptyStackException();
-                } else {
-                    while (!stack.empty()) {
-                        queue.push(stack.pop());
-                    }
+                while (!stack.empty()) {
+                    queue.push(stack.pop());
                 }
             }
             return queue.pop();
@@ -82,13 +71,9 @@ public class _232_ImplementQueueUsingStacks {
 
         public int peek() {
             if (queue.empty()) {
-//                if (stack.empty()) {
-//                    throw new EmptyStackException();
-//                } else {
-                    while (!stack.empty()) {
-                        queue.push(stack.pop());
-                    }
-//                }
+                while (!stack.empty()) {
+                    queue.push(stack.pop());
+                }
             }
             return queue.peek();
         }

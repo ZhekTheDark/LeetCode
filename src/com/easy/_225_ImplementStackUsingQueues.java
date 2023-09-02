7b15e8e27e054c3bd50b5d47b1f2e588682b2459
Zhek <Zhekfake@gmail.com>
2023-09-02 22:07:23 +0300

    _225_ImplementStackUsingQueues working

--- src/com/easy/_225_ImplementStackUsingQueues.java
+++ src/com/easy/_225_ImplementStackUsingQueues.java
@@ -1,4 +1,27 @@
 package com.easy;
 
 public class _225_ImplementStackUsingQueues {
+
+    class MyStack {
+
+        public MyStack() {
+
+        }
+
+        public void push(int x) {
+
+        }
+
+        public int pop() {
+
+        }
+
+        public int top() {
+
+        }
+
+        public boolean empty() {
+
+        }
+    }
 }

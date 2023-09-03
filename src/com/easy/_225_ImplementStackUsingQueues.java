d0c4ad8ac60eddbd8725d1ac8207b5073eca072d
Zhek <Zhekfake@gmail.com>
2023-09-03 21:37:18 +0300

    _225_ImplementStackUsingQueues done

--- src/com/easy/_225_ImplementStackUsingQueues.java
+++ src/com/easy/_225_ImplementStackUsingQueues.java
@@ -1,27 +1,35 @@
 package com.easy;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class _225_ImplementStackUsingQueues {
 
     class MyStack {
+
+        Queue<Integer> queue = new LinkedList();
 
         public MyStack() {
 
         }
 
         public void push(int x) {
-
+            queue.add(x);
+            for (int i = 0; i < queue.size() - 1; i++) {
+                queue.add(queue.poll());
+            }
         }
 
         public int pop() {
-
+            return queue.poll();
         }
 
         public int top() {
-
+            return queue.peek();
         }
 
         public boolean empty() {
-
+            return queue.isEmpty();
         }
     }
 }

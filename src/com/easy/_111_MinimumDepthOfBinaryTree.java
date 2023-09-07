34535915ffe68aae2aaebcd0c009e1d8edc42802
Zhek <Zhekfake@gmail.com>
2023-09-07 20:59:33 +0300

    _111_MinimumDepthOfBinaryTree.java added

---
+++ src/com/easy/_111_MinimumDepthOfBinaryTree.java
@@ -0,0 +1,11 @@
+package com.easy;
+
+import static com.easy._94_BinaryTreeInorderTraversal.*;
+
+public class _111_MinimumDepthOfBinaryTree {
+
+    public int minDepth(TreeNode root) {
+
+
+    }
+}

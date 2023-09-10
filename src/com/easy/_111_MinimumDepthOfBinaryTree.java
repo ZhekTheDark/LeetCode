bca75ba3385faee8afc89450fec302e401979b57
Zhek <Zhekfake@gmail.com>
2023-09-10 20:00:56 +0300

    _111_MinimumDepthOfBinaryTree added description

--- src/com/easy/_111_MinimumDepthOfBinaryTree.java
+++ src/com/easy/_111_MinimumDepthOfBinaryTree.java
@@ -1,7 +1,24 @@
 package com.easy;
 
-import static com.easy._94_BinaryTreeInorderTraversal.*;
+import static com.easy._94_BinaryTreeInorderTraversal.TreeNode;
 
+/**
+ * Given the root of a binary tree, return its maximum depth.
+ * <p>
+ * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.aa
+ * <p>
+ * Example 1:
+ * Input: root = [3,9,20,null,null,15,7]
+ * Output: 3
+ * <p>
+ * Example 2:
+ * Input: root = [1,null,2]
+ * Output: 2
+ * <p>
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 10^4].
+ * -100 <= Node.val <= 100
+ */
 public class _111_MinimumDepthOfBinaryTree {
 
     int max = Integer.MAX_VALUE;

354769daab4edd6999d9762b88cda393d066ca71
Zhek <Zhekfake@gmail.com>
2023-09-11 21:45:27 +0300

    _1026_MaximumDifferenceBetweenNodeAndAncestor.java added

---
+++ src/com/medium/_1026_MaximumDifferenceBetweenNodeAndAncestor.java
@@ -0,0 +1,28 @@
+package com.medium;
+
+import static com.easy._94_BinaryTreeInorderTraversal.TreeNode;
+
+public class _1026_MaximumDifferenceBetweenNodeAndAncestor {
+
+    int max = 0;
+
+    public int maxAncestorDiff(TreeNode root) {
+        getDiff(root, root.val, root.val);
+        return max;
+    }
+
+    public void getDiff(TreeNode node, int minAncestorValue, int maxAncestorValue) {
+        if (node == null) {
+            return;
+        }
+        int minDiff = Math.abs(minAncestorValue - node.val);
+        int maxDiff = Math.abs(maxAncestorValue - node.val);
+        max = Math.max(minDiff, max);
+        max = Math.max(maxDiff, max);
+
+        int minValue = Math.min(minAncestorValue, node.val);
+        int maxValue = Math.max(maxAncestorValue, node.val);
+        getDiff(node.left, minValue, maxValue);
+        getDiff(node.right, minValue, maxValue);
+    }
+}

06b40f85dc84f3be03bf64555f770cddd9352f50
Zhek <Zhekfake@gmail.com>
2023-09-14 22:28:21 +0300

    _543_DiameterOfBinaryTree.java added

---
+++ src/com/easy/_543_DiameterOfBinaryTree.java
@@ -0,0 +1,29 @@
+package com.easy;
+
+import com.easy._94_BinaryTreeInorderTraversal.TreeNode;
+
+public class _543_DiameterOfBinaryTree {
+
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return diameter;
+    }
+
+    public int depth(TreeNode node) {
+        if (node.left == null && node.right == null) {
+            return 1;
+        }
+        int leftDepth = 0;
+        int rightDepth = 0;
+        if (node.left != null) {
+            leftDepth = depth(node.left);
+        }
+        if (node.right != null) {
+            rightDepth = depth(node.right);
+        }
+        diameter = Math.max(diameter, leftDepth + rightDepth + 1);
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}

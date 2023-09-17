c537682cc6381eb2ef301334498fed7fe412cc51
Zhek <Zhekfake@gmail.com>
2023-09-17 22:33:17 +0300

    _707_DesignLinkedList.java added

---
+++ src/com/medium/_707_DesignLinkedList.java
@@ -0,0 +1,31 @@
+package com.medium;
+
+public class _707_DesignLinkedList {
+
+    class MyLinkedList {
+
+        public MyLinkedList() {
+
+        }
+
+        public int get(int index) {
+
+        }
+
+        public void addAtHead(int val) {
+
+        }
+
+        public void addAtTail(int val) {
+
+        }
+
+        public void addAtIndex(int index, int val) {
+
+        }
+
+        public void deleteAtIndex(int index) {
+
+        }
+    }
+}

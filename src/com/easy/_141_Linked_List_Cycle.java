2bc3e95f7e21ce36fdc4a926fc3123fd1e059438
Zhek <Zhekfake@gmail.com>
2023-09-27 21:16:35 +0300

    _141_Linked_List_Cycle done

--- src/com/easy/_141_Linked_List_Cycle.java
+++ src/com/easy/_141_Linked_List_Cycle.java
@@ -2,7 +2,34 @@ package com.easy;
 
 public class _141_Linked_List_Cycle {
 
-    public boolean hasCycle(ListNode head) {
+    class ListNode {
+        int val;
+        ListNode next;
 
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        if (if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode pointer1 = head;
+        ListNode pointer2 = head;
+        while (pointer2.next != null) {
+            pointer1 = pointer1.next;
+            pointer2 = pointer2.next;
+            if (pointer2.next == null) {
+                return false;
+            } else {
+                pointer2 = pointer2.next;
+            }
+            if (pointer1 == pointer2) {
+                return true;
+            }
+        }
+        return false;
     }
 }

75f5efd8f13463e409357e47dd0e032120cda6e8
Zhek <Zhekfake@gmail.com>
2023-10-01 20:09:22 +0300

    _142_LinkedListCycleII working

--- src/com/medium/_142_LinkedListCycleII.java
+++ src/com/medium/_142_LinkedListCycleII.java
@@ -1,4 +1,18 @@
 package com.medium;
 
 public class _142_LinkedListCycleII {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode detectCycle(ListNode head) {
+        return null;
+    }
 }

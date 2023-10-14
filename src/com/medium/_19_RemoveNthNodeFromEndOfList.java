caa3839a8bf9988cdded86b9d6e96d2183a322f2
Zhek <Zhekfake@gmail.com>
2023-10-14 19:48:32 +0300

    _19_RemoveNthNodeFromEndOfList.java added

---
+++ src/com/medium/_19_RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,48 @@
+package com.medium;
+
+public class _19_RemoveNthNodeFromEndOfList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head.next == null) {
+            return null;
+        }
+
+        int i = 0;
+        ListNode node = head;
+        while (n - i > 0) {
+            node = node.next;
+            i++;
+        }
+        ListNode target = head;
+
+        if (node.next == null) {
+            target = target.next;
+            return target;
+        }
+
+        while (node.next != null) {
+            node = node.next;
+            target = target.next;
+        }
+
+        target.next = target.next.next;
+        return head;
+    }
+}

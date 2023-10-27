3842f25aa8a5f5f53056aa46b6b164aefba31eeb
Zhek <Zhekfake@gmail.com>
2023-10-27 22:17:32 +0300

    _203_RemoveLinkedListElements working

--- src/com/easy/_203_RemoveLinkedListElements.java
+++ src/com/easy/_203_RemoveLinkedListElements.java
@@ -1,4 +1,26 @@
 package com.easy;
 
 public class _203_RemoveLinkedListElements {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+
+        return head;
+    }
 }

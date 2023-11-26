d7cb017d8f3f118395fc54eff99a4f32423389ec
Zhek <Zhekfake@gmail.com>
2023-11-26 20:21:02 +0300

    _328_OddEvenLinkedList working

--- src/com/medium/_328_OddEvenLinkedList.java
+++ src/com/medium/_328_OddEvenLinkedList.java
@@ -3,22 +3,21 @@ package com.medium;
 /**
  * Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even
  * indices, and return the reordered list.
- *
+ * <p>
  * The first node is considered odd, and the second node is even, and so on.
- *
+ * <p>
  * Note that the relative order inside both the even and odd groups should remain as it was in the input.
- *
+ * <p>
  * You must solve the problem in O(1) extra space complexity and O(n) time complexity.
- *
+ * <p>
  * Example 1:
  * Input: head = [1,2,3,4,5]
  * Output: [1,3,5,2,4]
- *
+ * <p>
  * Example 2:
  * Input: head = [2,1,3,5,6,4,7]
  * Output: [2,3,6,7,1,5,4]
- *
- *
+ * <p>
  * Constraints:
  * The number of nodes in the linked list is in the range [0, 10^4].
  * -10^6 <= Node.val <= 10^6
